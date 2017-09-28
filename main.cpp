0b1c5117b79c3e1a9f9d7bc9f175cb2c4a252d72
Bartlomiej Antosik <antosik.bartek@gmail.com>
2017-09-28 18:15:36 +0200

    Working example of runtime concept based polymorphism

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 class object_t;
 
-void draw(const int& x, ostream& out, size_t position)
+template<typename T>
+void draw(const T& x, ostream& out, size_t position)
 {
     out << string(position, ' ') << x << endl;
 }
@@ -17,12 +18,14 @@ void draw(const int& x, ostream& out, size_t position)
 class object_t
 {
 public:
-    object_t(int x) : self_(make_unique<int_model_t>(move(x))) 
+    template <typename T>
+    object_t(T x) : self_(make_unique<model<T>>(move(x))) 
     {
     }
     //this time we have to write our own copy ctor and assign operator as the compiler constructed is not 
     //enough (copies won't be disjoint if copied using compile supplied ctor)
-    object_t(const object_t& o) : self_(make_unique<int_model_t>(*o.self_))
+    //when going to many types we have to virtualize copy on the concept
+    object_t(const object_t& o) : self_(o.self_->copy_())
     {
     }
     //not all c++11 compliant compilers will treat operator=(object_t) as a cause to elide copy when object_t is copied in enclosing data structure
@@ -37,15 +40,27 @@ public:
     friend void draw(const object_t& x, ostream& out, size_t position)
     { x.self_->draw_(out, position); }
 private:
-    struct int_model_t {
-        int_model_t(int x) : data_(move(x)) {}
-        void draw_(ostream& out, size_t position) const
+    struct concept_t {
+        virtual ~concept_t() = default;
+        virtual void draw_(ostream&, size_t) const = 0;
+        virtual unique_ptr<concept_t> copy_() const = 0;
+    };
+    template <typename T>
+    struct model final : concept_t {
+        model(T x) : data_(move(x)) {}
+        unique_ptr<concept_t> copy_() const override
+        {
+            return make_unique<model>(*this);
+        }
+        
+        void draw_(ostream& out, size_t position) const override
         {
             draw(data_, out, position);
         }
-        int data_;
+        T data_;
     };
-    unique_ptr<int_model_t> self_;
+
+    unique_ptr<concept_t> self_;
 };
 
 using document_t = vector<object_t>;
@@ -67,6 +82,15 @@ some_t func()
     return { 5 };
 }
 
+class my_class_t {
+
+};
+
+void draw(const my_class_t&, ostream& out, size_t position)
+{
+    out << string(position, ' ') << "my_class_t" << endl;
+}
+
 int main()
 {
     cout << "some_t test start" << endl;
@@ -77,9 +101,11 @@ int main()
     document.reserve(5);
        
     document.emplace_back(0);
-    document.emplace_back(1);
+    //after adding string_model_t we can  add string to the structure
+    document.emplace_back(string("Hello!"));
     document.emplace_back(2);
-    document.emplace_back(3);
+    document.emplace_back(my_class_t());
+    document.emplace_back(document);
     reverse(document.begin(), document.end());
 
     draw(document, cout, 0);
