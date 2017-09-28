1a42c7163e144fb84266a48db4e350ab6a345559
Bartlomiej Antosik <antosik.bartek@gmail.com>
2017-09-28 18:48:24 +0200

    Removing spurious copies in history solution

--- main.cpp
+++ main.cpp
@@ -20,41 +20,21 @@ class object_t
 {
 public:
     template <typename T>
-    object_t(T x) : self_(make_unique<model<T>>(move(x))) 
+    object_t(T x) : self_(make_shared<model<T>>(move(x))) 
     {
     }
-    //this time we have to write our own copy ctor and assign operator as the compiler constructed is not 
-    //enough (copies won't be disjoint if copied using compile supplied ctor)
-    //when going to many types we have to virtualize copy on the concept
-    object_t(const object_t& o) : self_(o.self_->copy_())
-    {
-        cout << "copy" << endl;
-    }
-    //not all c++11 compliant compilers will treat operator=(object_t) as a cause to elide copy when object_t is copied in enclosing data structure
-    object_t& operator=(const object_t& x)
-    {
-        return *this = object_t(x);
-    }
-    object_t& operator=(object_t&&) noexcept = default;
-    //to benefit from move semantics related algos optimization we have to supply our own move constructor as it is not 
-    //created
-    object_t(object_t&& x) noexcept = default;
+   
     friend void draw(const object_t& x, ostream& out, size_t position)
     { x.self_->draw_(out, position); }
 private:
     struct concept_t {
         virtual ~concept_t() = default;
         virtual void draw_(ostream&, size_t) const = 0;
-        virtual unique_ptr<concept_t> copy_() const = 0;
     };
     template <typename T>
     struct model final : concept_t {
         model(T x) : data_(move(x)) {}
-        unique_ptr<concept_t> copy_() const override
-        {
-            return make_unique<model>(*this);
-        }
-        
+
         void draw_(ostream& out, size_t position) const override
         {
             draw(data_, out, position);
@@ -62,7 +42,7 @@ private:
         T data_;
     };
 
-    unique_ptr<concept_t> self_;
+    shared_ptr<const concept_t> self_;
 };
 
 using document_t = vector<object_t>;
