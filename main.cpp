e42d71511d9b1482db697192fbd3538db9ca223a
Bartlomiej Antosik <antosik.bartek@gmail.com>
2017-09-28 17:07:01 +0200

    Adding a unique_ptr to int wrapper in object_t class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
@@ -11,15 +12,33 @@ void draw(const int& x, ostream& out, size_t position)
     out << string(position, ' ') << x << endl;
 }
 
-// by creating classes that behave like regular objects we can 
+// by creating classes that behave like regular objects we can make them more reusable
 class object_t
 {
 public:
-    object_t(const int& x) : self_(x) {}
+    object_t(const int& x) : self_(make_unique<int_model_t>(x)) {}
+    //this time we have to write our own copy ctor and assign operator as the compiler constructed is not 
+    //enough (copies won't be disjoint if copied using compile supplied ctor)
+    object_t(const object_t& o) : self_(make_unique<int_model_t>(*o.self_)) {}
+
+    object_t& operator=(const object_t& o)
+    {
+        object_t tmp(o);
+        self_ = move(tmp.self_);
+        return *this;
+    }
     friend void draw(const object_t& x, ostream& out, size_t position)
-    { draw(x.self_, out, position); }
+    { x.self_->draw_(out, position); }
 private:
-    int self_;
+    struct int_model_t {
+        int_model_t(const int& x) : data_(x) {}
+        void draw_(ostream& out, size_t position) const
+        {
+            draw(data_, out, position);
+        }
+        int data_;
+    };
+    unique_ptr<int_model_t> self_;
 };
 
 using document_t = vector<object_t>;
